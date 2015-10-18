12d7ae8866b11311abc2376deb7d20f53017094d
Serge <spbsves@gmail.com>
2015-10-18 18:22:12 -0400

    Auto Generated Test Add

---
+++ src/test/com/mycom/graph/GraphTest.java
@@ -0,0 +1,75 @@
+package test.com.mycom.graph; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Graph Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Oct 18, 2015</pre> 
+* @version 1.0 
+*/ 
+public class GraphTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getGraph() 
+* 
+*/ 
+@Test
+public void testGetGraph() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: isEmpty() 
+* 
+*/ 
+@Test
+public void testIsEmpty() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addEdge(V src, Node<V> dest) 
+* 
+*/ 
+@Test
+public void testAddEdgeForSrcDest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addEdge(V src, V dest, int weight) 
+* 
+*/ 
+@Test
+public void testAddEdgeForSrcDestWeight() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: debugPrint() 
+* 
+*/ 
+@Test
+public void testDebugPrint() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/com/mycom/graph/NodeTest.java
@@ -0,0 +1,45 @@
+package test.com.mycom.graph; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Node Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Oct 18, 2015</pre> 
+* @version 1.0 
+*/ 
+public class NodeTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getName() 
+* 
+*/ 
+@Test
+public void testGetName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getWeight() 
+* 
+*/ 
+@Test
+public void testGetWeight() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
