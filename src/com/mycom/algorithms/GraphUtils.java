4962ff2fc6ed2582beb336d91bc6edf774313445
Serge <spbsves@gmail.com>
2015-10-18 20:28:51 -0400

    Start to work on utilites

---
+++ src/com/mycom/algorithms/GraphUtils.java
@@ -0,0 +1,46 @@
+package com.mycom.algorithms;
+
+import com.mycom.graph.Graph;
+import com.mycom.graph.Node;
+
+import java.util.*;
+
+
+/**
+ * Created by sveselkov on 10/18/2015.
+ */
+public class GraphUtils<V> {
+
+    /*Dijkstra algorithm
+    * General O(n*n) algorithm but here may be O(n*n*n) due to collection realisation
+    * Not finished
+    * */
+    public Map<V,Integer> shortPath ( Graph<V> graph, V src){
+        Map<V,Integer> shortPathes= new HashMap<>();
+        Map<V, List<Node<V>>> adgList = graph.getGraph();
+        Set<V> vertices = graph.getVertices();
+
+        // Init distances
+        for ( V v: vertices){
+            if ( v == src){
+                shortPathes.put(v, 0);
+            } else {
+                shortPathes.put(v, Integer.MAX_VALUE);
+            }
+        }
+        V curr_v = src;
+        vertices.remove( src );
+
+        do {
+            List<Node<V>> adjVertices = adgList.get(curr_v);
+            for ( Node<V> node: adjVertices){
+                Integer pathLength = node.getWeight() + shortPathes.get( curr_v);
+                if ( pathLength < shortPathes.get(node.getName())){
+                    shortPathes.put(node.getName(),pathLength);
+
+                }
+            }
+        } while ( vertices.size() > 0);
+        return ( shortPathes);
+    }
+}
